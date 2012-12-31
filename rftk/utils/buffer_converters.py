9076772646d609040b6849389e8cdd9ea41ff5fe
David Matheson <david.j.matheson@gmail.com>
2012-12-31 14:45:19 -0800

    Buffer converter helpers to go from numpy arrays to MatrixBufferInt, MatrixBufferFloat, ImgBufferInt, ImgBufferFloat and back to numpy arrays. Includes unit tests that verify all code paths.

---
+++ rftk/utils/buffer_converters.py
@@ -0,0 +1,75 @@
+import numpy as np
+import rftk.buffers as buffers
+
+def as_img_buffer( np_array ):
+    if np_array.dtype == np.int32 and np_array.ndim == 2:
+        return buffers.imgBufferInt( np_array )
+    elif np_array.dtype == np.int32 and np_array.ndim == 3:
+        return buffers.imgsBufferInt( np_array )
+    elif np_array.dtype == np.float32 and np_array.ndim == 2:
+        return buffers.imgBufferFloat( np_array )
+    elif np_array.dtype == np.float32 and np_array.ndim == 3:
+        return buffers.imgsBufferFloat( np_array )
+    elif np_array.dtype == np.int64 and np_array.ndim == 2:
+        return buffers.imgBufferInt64( np_array )
+    elif np_array.dtype == np.int64 and np_array.ndim == 3:
+        return buffers.imgsBufferInt64( np_array )
+    elif np_array.dtype == np.float64 and np_array.ndim == 2:
+        return buffers.imgBufferFloat64( np_array )
+    elif np_array.dtype == np.float64 and np_array.ndim == 3:
+        return buffers.imgsBufferFloat64( np_array )
+    else:
+        raise Exception('asImgBuffer unknown type and ndim', np_array.dtype, np_array.ndim())
+
+def as_matrix_buffer( np_array ):
+    if np_array.dtype == np.int32 and np_array.ndim == 1:
+        return buffers.vecBufferInt( np_array )
+    elif np_array.dtype == np.int32 and np_array.ndim == 2:
+        return buffers.matrixBufferInt( np_array )
+    elif np_array.dtype == np.float32 and np_array.ndim == 1:
+        return buffers.vecBufferFloat( np_array )
+    elif np_array.dtype == np.float32 and np_array.ndim == 2:
+        return buffers.matrixBufferFloat( np_array )
+    elif np_array.dtype == np.int64 and np_array.ndim == 1:
+        return buffers.vecBufferInt64( np_array )
+    elif np_array.dtype == np.int64 and np_array.ndim == 2:
+        return buffers.matrixBufferInt64( np_array )
+    elif np_array.dtype == np.float64 and np_array.ndim == 1:
+        return buffers.vecBufferFloat64( np_array )
+    elif np_array.dtype == np.float64 and np_array.ndim == 2:
+        return buffers.matrixBufferFloat64( np_array )
+    else:
+        raise Exception('asMatrixBuffer unknown type and ndim', np_array.dtype, np_array.ndim())
+
+def as_numpy_array( buffer, flatten=False ):
+    isImgBufferFloat = isinstance(buffer, buffers.ImgBufferFloat)
+    isImgBufferInt = isinstance(buffer, buffers.ImgBufferInt)
+    isMatrixBufferFloat = isinstance(buffer, buffers.MatrixBufferFloat)
+    isMatrixBufferInt = isinstance(buffer, buffers.MatrixBufferInt)
+    
+    assert(isImgBufferFloat or isImgBufferInt or isMatrixBufferFloat or isMatrixBufferInt)
+
+    if isImgBufferFloat or isMatrixBufferFloat:
+        buffer_type = np.float32
+    else:
+        buffer_type = np.int32
+
+    if isImgBufferFloat or isImgBufferInt:
+        result = np.zeros((buffer.GetNumberOfImgs(), buffer.GetM(), buffer.GetN()), dtype=buffer_type)
+        buffer.AsNumpy(result)
+        if buffer.GetNumberOfImgs() == 1 and flatten:
+            result = result.reshape(buffer.GetM(), buffer.GetN)
+
+    elif isMatrixBufferFloat or isMatrixBufferInt:
+        result = np.zeros((buffer.GetM(), buffer.GetN()), dtype=buffer_type)
+        buffer.AsNumpy(result)
+        if buffer.GetN() == 1 and flatten:
+            result = result.flatten()
+
+    return result
+
+
+
+
+
+
---
+++ tests/test_buffer_converters.py
@@ -0,0 +1,85 @@
+import unittest as unittest
+import numpy as np
+import rftk.buffers as buffers
+import rftk.utils.buffer_converters as buffer_converters
+
+class TestBuffers(unittest.TestCase):
+
+    def convert_img_buffer_both_directions_helper(self, X, buffer_type):
+        buf = buffer_converters.as_img_buffer(X)
+        assert isinstance(buf, buffer_type)
+        X_back = buffer_converters.as_numpy_array(buf)
+        self.assertTrue((X == X_back).all())
+
+    def img_buffer_flatten_helper(self, X, buffer_type):  
+        m,n = X.shape       
+        img_buffer = buffer_converters.as_img_buffer(X)
+        assert isinstance(img_buffer, buffer_type)
+        X_back = buffer_converters.as_numpy_array(img_buffer)
+        self.assertTrue((X.reshape(1,m,n) == X_back).all())
+        X_back = buffer_converters.as_numpy_array(buffer=img_buffer, flatten=True)
+        self.assertTrue((X == X_back).all())
+
+    def convert_matrix_buffer_both_directions_helper(self, X, buffer_type):
+        buf = buffer_converters.as_matrix_buffer(X)
+        assert isinstance(buf, buffer_type)
+        X_back = buffer_converters.as_numpy_array(buf)
+        self.assertTrue((X == X_back).all())
+
+    def matrix_buffer_flatten_helper(self, X, buffer_type):     
+        n = len(X.shape)   
+        matrix_buffer = buffer_converters.as_matrix_buffer(X)
+        assert isinstance(matrix_buffer, buffer_type)
+        X_back = buffer_converters.as_numpy_array(matrix_buffer)
+        self.assertTrue((X == X_back.flatten()).all())
+        X_back = buffer_converters.as_numpy_array(buffer=matrix_buffer, flatten=True)
+        self.assertTrue((X == X_back).all())
+
+    def test_img_buffer(self):
+        X = np.array([[[3,21,1],[22,1,5]],[[2,3,4],[7,7,7]]], dtype=np.int32 )
+        self.convert_img_buffer_both_directions_helper(X=X, buffer_type=buffers.ImgBufferInt)
+        X = np.array([[[3,21,1],[22,1,5]],[[2,3,4],[7,7,7]]], dtype=np.int )
+        self.convert_img_buffer_both_directions_helper(X=X, buffer_type=buffers.ImgBufferInt)
+        X = np.array([[[3,21,1],[22,1,5]],[[2,3,4],[7,7,7]]], dtype=np.float32 )
+        self.convert_img_buffer_both_directions_helper(X=X, buffer_type=buffers.ImgBufferFloat)
+        X = np.array([[[3,21,1],[22,1,5]],[[2,3,4],[7,7,7]]], dtype=np.float )
+        self.convert_img_buffer_both_directions_helper(X=X, buffer_type=buffers.ImgBufferFloat)
+        
+
+    def test_img_buffer_flatten(self):
+        X = np.array([[3,21,1],[22,1,5]], dtype=np.int32 )
+        self.img_buffer_flatten_helper(X=X, buffer_type=buffers.ImgBufferInt)
+        X_64 = np.array([[3,21,1],[22,1,5]], dtype=np.int )
+        self.img_buffer_flatten_helper(X=X_64, buffer_type=buffers.ImgBufferInt)
+        X = np.array([[3,21,1],[22,1,5]], dtype=np.float32 )
+        self.img_buffer_flatten_helper(X=X, buffer_type=buffers.ImgBufferFloat)
+        X_64 = np.array([[3,21,1],[22,1,5]], dtype=np.float )
+        self.img_buffer_flatten_helper(X=X_64, buffer_type=buffers.ImgBufferFloat)
+
+    def test_matrix_buffer(self):
+        X = np.array([[3,21,1],[22,1,5]], dtype=np.int32 )
+        self.convert_matrix_buffer_both_directions_helper(X=X, buffer_type=buffers.MatrixBufferInt)
+        X = np.array([[3,21,1],[22,1,5]], dtype=np.int )
+        self.convert_matrix_buffer_both_directions_helper(X=X, buffer_type=buffers.MatrixBufferInt)
+        X = np.array([[3,21,1],[22,1,5]], dtype=np.float32 )
+        self.convert_matrix_buffer_both_directions_helper(X=X, buffer_type=buffers.MatrixBufferFloat)
+        X = np.array([[3,21,1],[22,1,5]], dtype=np.float )
+        self.convert_matrix_buffer_both_directions_helper(X=X, buffer_type=buffers.MatrixBufferFloat)
+        
+
+    def test_matrix_buffer_flatten(self):
+        X = np.array([22,1,5], dtype=np.int32 )
+        self.matrix_buffer_flatten_helper(X=X, buffer_type=buffers.MatrixBufferInt)
+        X_64 = np.array([22,1,5], dtype=np.int )
+        self.matrix_buffer_flatten_helper(X=X_64, buffer_type=buffers.MatrixBufferInt)
+        X = np.array([22,1,5], dtype=np.float32 )
+        self.matrix_buffer_flatten_helper(X=X, buffer_type=buffers.MatrixBufferFloat)
+        X_64 = np.array([22,1,5], dtype=np.float )
+        self.matrix_buffer_flatten_helper(X=X_64, buffer_type=buffers.MatrixBufferFloat)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
