d662aaeae65774639040fe984804ed62d2d9c53d
David Matheson <david.j.matheson@gmail.com>
2013-01-04 02:00:06 -0800

    Renamed STANDARD_FEATURE_... to VEC_FEATURE_... to be consistent

--- rftk-native/feature_extractors/AxisAlignedFeatureExtractor.h
+++ rftk-native/feature_extractors/AxisAlignedFeatureExtractor.h
@@ -12,7 +12,7 @@ public:
 
   ~AxisAlignedFeatureExtractor() {}
 
-  virtual int GetUID() { return STANDARD_FEATURE_AXIS_ALIGNED; }
+  virtual int GetUID() { return VEC_FEATURE_AXIS_ALIGNED; }
 
   virtual void Extract( const MatrixBufferInt& sampleIndices,
                         const MatrixBufferInt& intFeatureParams,
