0336e4ce2f57483c94aa90be73c8986bfc4a9c22
David Matheson <david.j.matheson@gmail.com>
2013-01-03 19:03:59 -0800

    Added new modules to the build all script

--- rftk-native/buildall.sh
+++ rftk-native/buildall.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 cd $PYTHONPATH/rftk-native/assert_util
 python setup.py build_ext --inplace -f rftkinstall
+cd $PYTHONPATH/rftk-native/bootstrap
+python setup.py build_ext --inplace -f rftkinstall
 cd $PYTHONPATH/rftk-native/buffers
 python setup.py build_ext --inplace -f rftkinstall
 cd $PYTHONPATH/rftk-native/features
 python setup.py build_ext --inplace -f rftkinstall
-
+cd $PYTHONPATH/rftk-native/feature_extractors
+python setup.py build_ext --inplace -f rftkinstall
+cd $PYTHONPATH/rftk-native/best_split
+python setup.py build_ext --inplace -f rftkinstall
