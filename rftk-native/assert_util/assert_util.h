1345db9cb06030bb4597b28f0a0f612f606c8f2b
David Matheson <david.j.matheson@gmail.com>
2013-01-11 14:46:47 -0800

    Add standard ASSERT

--- rftk-native/assert_util/assert_util.h
+++ rftk-native/assert_util/assert_util.h
@@ -6,23 +6,27 @@
 #if ENABLE_EXCEPTIONS
 
 #include <string>
-void AssertArgDimension1d(int n_lhs, int n_rhs, 
+void AssertArgDimension1d(int n_lhs, int n_rhs,
                             const std::string& file,
                             const int line);
 
-void AssertArgDimension2d(  int m_lhs, int n_lhs, 
-                            int m_rhs, int n_rhs, 
+void AssertArgDimension2d(  int m_lhs, int n_lhs,
+                            int m_rhs, int n_rhs,
                             const std::string& file,
                             const int line);
 
-void AssertArgDimension3d(  int l_lhs, int m_lhs, int n_lhs, 
-                            int l_rhs, int m_rhs, int n_rhs, 
+void AssertArgDimension3d(  int l_lhs, int m_lhs, int n_lhs,
+                            int l_rhs, int m_rhs, int n_rhs,
                             const std::string& file,
                             const int line);
 
 void AssertValidRange( int index, int range_low, int range_high,
                         const std::string& file,
                         const int line);
+
+void Assert(    bool test,
+                const std::string& file,
+                const int line);
 
 #define ASSERT_ARG_DIM_1D(n_lhs, n_rhs) \
                         AssertArgDimension1d(n_lhs, n_rhs,  __FILE__, __LINE__);
@@ -36,12 +40,15 @@ void AssertValidRange( int index, int range_low, int range_high,
 #define ASSERT_VALID_RANGE(index, range_low, range_high) \
                         AssertValidRange(index, range_low, range_high, __FILE__, __LINE__);
 
+#define ASSERT(test) Assert(test, __FILE__, __LINE__);
+
 #else
 
-#define ASSERT_ARG_DIM_1D(n_lhs, n_rhs) 
+#define ASSERT_ARG_DIM_1D(n_lhs, n_rhs)
 #define ASSERT_ARG_DIM_2D(m_lhs, n_lhs, m_rhs, n_rhs)
 #define ASSERT_ARG_DIM_3D(l_lhs, m_lhs, n_lhs, l_rhs, m_rhs, n_rhs)
 #define ASSERT_VALID_RANGE(index, range_low, range_high)
+#define ASSERT(test)
 
 #endif
 
