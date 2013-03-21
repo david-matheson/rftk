4de1cb6586d811f91e910253a8c246a5b7ea6585
David Matheson <david.j.matheson@gmail.com>
2013-03-20 18:38:14 -0700

    Buffer collection: -Added sparse matrices to the buffer collection -Tests for adding and getting all data types (float32, int64, ...) and container types (Vector, Matrix, ...) -Generate all buffer type names rather than listing them hard coding them -Renamed macros from LEGACY to SWIG

--- modules/buffers/buffer_collection.i
+++ modules/buffers/buffer_collection.i
@@ -1,4 +1,5 @@
 %pythoncode %{
+import itertools
 import buffers as buffers
 
 def _buffer_add(self, name, b):
@@ -12,43 +13,32 @@ def _buffer_add(self, name, b):
     else:
         raise Exception('BufferCollection.Add failed because %s does not exist' % function_name)
 
-BufferCollection.Add = _buffer_add
+BufferCollection.AddBuffer = _buffer_add
 
-_buffer_type_names = ['Float32VectorBuffer', 
-                'Float64VectorBuffer', 
-                'Int32VectorBuffer', 
-                'Int64VectorBuffer',
-                'Float32MatrixBuffer', 
-                'Float64MatrixBuffer', 
-                'Int32MatrixBuffer', 
-                'Int64MatrixBuffer',
-                'Float32Tensor3Buffer', 
-                'Float64Tensor3Buffer', 
-                'Int32VectorTensor3Buffer', 
-                'Int64VectorTensor3Buffer',
-                'Float32SparseMatrixBuffer', 
-                'Float64SparseMatrixBuffer', 
-                'Int32VectorSparseMatrixBuffer', 
-                'Int64VectorSparseMatrix']
 
 def _get_buffer(self, name):
-    for buffer_type_name in _buffer_type_names:
-        has_function_name = "%s%s" % ('Has', buffer_type_name)
+    data_type_list = ['Float32', 'Float64', 'Int32', 'Int64']
+    container_type_list = ['Vector', 'Matrix', 'SparseMatrix', 'Tensor3']
+
+    for (data_type, container_type) in itertools.product(data_type_list, container_type_list):
+        buffer_type_name = "%s%sBuffer" % (data_type, container_type)
+        has_function_name = "Has%s" % buffer_type_name
         has_function = getattr(self, has_function_name)
         if has_function(name):
-            get_function_name = "%s%s" % ('Get', buffer_type_name)
+            get_function_name = "Get%s" % buffer_type_name
             get_function = getattr(self, get_function_name)
             return get_function(name)
     raise Exception('BufferCollection.GetBuffer failed because %s does not exist' % name)
 
 BufferCollection.GetBuffer = _get_buffer
 
+
 buffers._BufferCollection = buffers.BufferCollection
 
 def _create_buffer(**kwargs):
     collection = buffers._BufferCollection()
     for key, value in kwargs.iteritems():
-        collection.Add(key, value)
+        collection.AddBuffer(key, value)
     return collection
 
 buffers.BufferCollection = _create_buffer
--- tests/test_buffer_collection.py
+++ tests/test_buffer_collection.py
@@ -6,78 +6,144 @@ import rftk.native.buffers as buffers
 
 class TestBufferCollectoin(unittest.TestCase):
 
-    def test_img_float(self):
-        collection = buffers.BufferCollection()
-
-        self.assertEqual(collection.HasBuffer("first"), False)
-        data_1 = np.array([[[3,21,1],[22,1,5]],[[2,3,4],[7,7,7]]], dtype=np.float32 )
-        collection.Add("first", data_1)
-        self.assertEqual(collection.HasBuffer("first"), True)
-
-        self.assertEqual(collection.HasFloat32Tensor3Buffer("second"), False)
-        data_2 = np.array([[[3,21,1],[22,1,5]]], dtype=np.float32 )
-        collection.Add("second", data_2)
-        self.assertEqual(collection.HasFloat32Tensor3Buffer("second"), True)
-
-        data_1_out = buffers.as_numpy_array(buffer=collection.GetBuffer("first"))
-        self.assertTrue((data_1 == data_1_out).all())
-
-        data_2_out = buffers.as_numpy_array(buffer=collection.GetBuffer("second"))
-        self.assertTrue((data_2 == data_2_out).all())
-
-    def test_img_int(self):
-        data_1 = np.array([[[3,21,1],[22,1,5]],[[2,3,4],[7,7,7]]], dtype=np.int32 )
+    def test_vector(self):
+        data_1 = np.array([3,21,1,22,1,5], dtype=np.float32)
         collection = buffers.BufferCollection(first=data_1)
 
-        # self.assertEqual(collection.HasInt32Tensor3Buffer("first"), False)
-        # collection.AddInt32Tensor3Buffer("first", buffers.as_tensor_buffer(data_1))
-        self.assertEqual(collection.HasInt32Tensor3Buffer("first"), True)
+        self.assertEqual(collection.HasBuffer("first"), True)
+        self.assertEqual(collection.HasFloat32VectorBuffer("first"), True)
+        self.assertEqual(collection.HasFloat64VectorBuffer("first"), False)
+        self.assertEqual(collection.HasInt32VectorBuffer("first"), False)
+        self.assertEqual(collection.HasInt64VectorBuffer("first"), False)
 
-        self.assertEqual(collection.HasInt32Tensor3Buffer("second"), False)
-        data_2 = np.array([[[3,21,1],[22,1,5]]], dtype=np.int32 )
-        collection.AddInt32Tensor3Buffer("second", buffers.as_tensor_buffer(data_2))
-        self.assertEqual(collection.HasInt32Tensor3Buffer("second"), True)
+        self.assertEqual(collection.HasBuffer("second"), False)
+        data_2 = np.array([3,21,1], dtype=np.float64 )
+        collection.AddBuffer("second", data_2)
+        self.assertEqual(collection.HasFloat32VectorBuffer("second"), False)
+        self.assertEqual(collection.HasFloat64VectorBuffer("second"), True)
+        self.assertEqual(collection.HasInt32VectorBuffer("second"), False)
+        self.assertEqual(collection.HasInt64VectorBuffer("second"), False)
 
-        data_1_out = buffers.as_numpy_array(buffer=collection.GetInt32Tensor3Buffer("first"))
+        self.assertEqual(collection.HasBuffer("third"), False)
+        data_3 = np.array([22,1,5], dtype=np.int32 )
+        collection.AddBuffer("third", data_3)
+        self.assertEqual(collection.HasFloat32VectorBuffer("third"), False)
+        self.assertEqual(collection.HasFloat64VectorBuffer("third"), False)
+        self.assertEqual(collection.HasInt32VectorBuffer("third"), True)
+        self.assertEqual(collection.HasInt64VectorBuffer("third"), False)
+
+        self.assertEqual(collection.HasBuffer("forth"), False)
+        data_4 = np.array([1,1,1], dtype=np.int64 )
+        collection.AddBuffer("forth", data_4)
+        self.assertEqual(collection.HasFloat32VectorBuffer("forth"), False)
+        self.assertEqual(collection.HasFloat64VectorBuffer("forth"), False)
+        self.assertEqual(collection.HasInt32VectorBuffer("forth"), False)
+        self.assertEqual(collection.HasInt64VectorBuffer("forth"), True)
+
+        data_1_out = buffers.as_numpy_array(collection.GetBuffer("first"))
         self.assertTrue((data_1 == data_1_out).all())
 
-        data_2_out = buffers.as_numpy_array(buffer=collection.GetInt32Tensor3Buffer("second"))
+        data_2_out = buffers.as_numpy_array(collection.GetBuffer("second"))
         self.assertTrue((data_2 == data_2_out).all())
 
-    def test_matrix_float(self):
-        collection = buffers.BufferCollection()
+        data_3_out = buffers.as_numpy_array(collection.GetBuffer("third"))
+        self.assertTrue((data_3 == data_3_out).all())
 
-        self.assertEqual(collection.HasFloat32MatrixBuffer("first"), False)
-        data_1 = np.array([[3,21,1],[22,1,5],[2,3,4],[7,7,7]], dtype=np.float32 )
-        collection.Add("first", data_1)
+        data_4_out = buffers.as_numpy_array(collection.GetBuffer("forth"))
+        self.assertTrue((data_4 == data_4_out).all())
+
+
+    def test_matrix(self):
+        data_1 = np.array([[3,21,1],[22,1,5],[2,3,4],[7,7,7]], dtype=np.float32)
+        collection = buffers.BufferCollection(first=data_1)
+
+        self.assertEqual(collection.HasBuffer("first"), True)
         self.assertEqual(collection.HasFloat32MatrixBuffer("first"), True)
-
-        self.assertEqual(collection.HasFloat32MatrixBuffer("second"), False)
-        data_2 = np.array([[3,21,1],[22,1,5]], dtype=np.float32 )
-        collection.Add("second", data_2)
-        self.assertEqual(collection.HasFloat32MatrixBuffer("second"), True)
-
-        data_1_out = buffers.as_numpy_array(buffer=collection.GetFloat32MatrixBuffer("first"))
-        self.assertTrue((data_1 == data_1_out).all())
-
-        data_2_out = buffers.as_numpy_array(buffer=collection.GetFloat32MatrixBuffer("second"))
-        self.assertTrue((data_2 == data_2_out).all())
-
-    def test_matrix_int(self):
-        collection = buffers.BufferCollection()
-
+        self.assertEqual(collection.HasFloat64MatrixBuffer("first"), False)
         self.assertEqual(collection.HasInt32MatrixBuffer("first"), False)
-        data_1 = np.array([[3,21,1],[22,1,5],[2,3,4],[7,7,7]], dtype=np.int32 )
-        collection.AddInt32MatrixBuffer("first", buffers.Int32Matrix2( data_1 ))
-        self.assertEqual(collection.HasInt32MatrixBuffer("first"), True)
+        self.assertEqual(collection.HasInt64MatrixBuffer("first"), False)
 
+        self.assertEqual(collection.HasBuffer("second"), False)
+        data_2 = np.array([[3,21,1],[22,1,5]], dtype=np.float64 )
+        collection.AddBuffer("second", data_2)
+        self.assertEqual(collection.HasFloat32MatrixBuffer("second"), False)
+        self.assertEqual(collection.HasFloat64MatrixBuffer("second"), True)
         self.assertEqual(collection.HasInt32MatrixBuffer("second"), False)
-        data_2 = np.array([[3,21,1],[22,1,5]], dtype=np.int32 )
-        collection.AddInt32MatrixBuffer("second", buffers.Int32Matrix2( data_2 ))
-        self.assertEqual(collection.HasInt32MatrixBuffer("second"), True)
+        self.assertEqual(collection.HasInt64MatrixBuffer("second"), False)
 
-        data_1_out = buffers.as_numpy_array(buffer=collection.GetInt32MatrixBuffer("first"))
+        self.assertEqual(collection.HasBuffer("third"), False)
+        data_3 = np.array([[3,21,1],[22,1,5]], dtype=np.int32 )
+        collection.AddBuffer("third", data_3)
+        self.assertEqual(collection.HasFloat32MatrixBuffer("third"), False)
+        self.assertEqual(collection.HasFloat64MatrixBuffer("third"), False)
+        self.assertEqual(collection.HasInt32MatrixBuffer("third"), True)
+        self.assertEqual(collection.HasInt64MatrixBuffer("third"), False)
+
+        self.assertEqual(collection.HasBuffer("forth"), False)
+        data_4 = np.array([[3,21,1],[1,1,1]], dtype=np.int64 )
+        collection.AddBuffer("forth", data_4)
+        self.assertEqual(collection.HasFloat32MatrixBuffer("forth"), False)
+        self.assertEqual(collection.HasFloat64MatrixBuffer("forth"), False)
+        self.assertEqual(collection.HasInt32MatrixBuffer("forth"), False)
+        self.assertEqual(collection.HasInt64MatrixBuffer("forth"), True)
+
+        data_1_out = buffers.as_numpy_array(collection.GetBuffer("first"))
         self.assertTrue((data_1 == data_1_out).all())
 
-        data_2_out = buffers.as_numpy_array(buffer=collection.GetInt32MatrixBuffer("second"))
+        data_2_out = buffers.as_numpy_array(collection.GetBuffer("second"))
         self.assertTrue((data_2 == data_2_out).all())
+
+        data_3_out = buffers.as_numpy_array(collection.GetBuffer("third"))
+        self.assertTrue((data_3 == data_3_out).all())
+
+        data_4_out = buffers.as_numpy_array(collection.GetBuffer("forth"))
+        self.assertTrue((data_4 == data_4_out).all())
+
+
+    def test_tensor3(self):
+        data_1 = np.array([[[3,21,1],[22,1,5]],[[2,3,4],[7,7,7]]], dtype=np.float32)
+        collection = buffers.BufferCollection(first=data_1)
+
+        self.assertEqual(collection.HasBuffer("first"), True)
+        self.assertEqual(collection.HasFloat32Tensor3Buffer("first"), True)
+        self.assertEqual(collection.HasFloat64Tensor3Buffer("first"), False)
+        self.assertEqual(collection.HasInt32Tensor3Buffer("first"), False)
+        self.assertEqual(collection.HasInt64Tensor3Buffer("first"), False)
+
+        self.assertEqual(collection.HasBuffer("second"), False)
+        data_2 = np.array([[[3,21,1],[22,1,5]]], dtype=np.float64 )
+        collection.AddBuffer("second", data_2)
+        self.assertEqual(collection.HasFloat32Tensor3Buffer("second"), False)
+        self.assertEqual(collection.HasFloat64Tensor3Buffer("second"), True)
+        self.assertEqual(collection.HasInt32Tensor3Buffer("second"), False)
+        self.assertEqual(collection.HasInt64Tensor3Buffer("second"), False)
+
+        self.assertEqual(collection.HasBuffer("third"), False)
+        data_3 = np.array([[[3,21,1]],[[22,1,5]]], dtype=np.int32 )
+        collection.AddBuffer("third", data_3)
+        self.assertEqual(collection.HasFloat32Tensor3Buffer("third"), False)
+        self.assertEqual(collection.HasFloat64Tensor3Buffer("third"), False)
+        self.assertEqual(collection.HasInt32Tensor3Buffer("third"), True)
+        self.assertEqual(collection.HasInt64Tensor3Buffer("third"), False)
+
+        self.assertEqual(collection.HasBuffer("forth"), False)
+        data_4 = np.array([[[3,21,1,1,1,1]]], dtype=np.int64 )
+        collection.AddBuffer("forth", data_4)
+        self.assertEqual(collection.HasFloat32Tensor3Buffer("forth"), False)
+        self.assertEqual(collection.HasFloat64Tensor3Buffer("forth"), False)
+        self.assertEqual(collection.HasInt32Tensor3Buffer("forth"), False)
+        self.assertEqual(collection.HasInt64Tensor3Buffer("forth"), True)
+
+        data_1_out = buffers.as_numpy_array(collection.GetBuffer("first"))
+        self.assertTrue((data_1 == data_1_out).all())
+
+        data_2_out = buffers.as_numpy_array(collection.GetBuffer("second"))
+        self.assertTrue((data_2 == data_2_out).all())
+
+        data_3_out = buffers.as_numpy_array(collection.GetBuffer("third"))
+        self.assertTrue((data_3 == data_3_out).all())
+
+        data_4_out = buffers.as_numpy_array(collection.GetBuffer("forth"))
+        self.assertTrue((data_4 == data_4_out).all())
+
+
