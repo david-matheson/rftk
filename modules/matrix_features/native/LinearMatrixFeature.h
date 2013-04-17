e2f1174f00487c9bf68a707912bd085b2311fb8b
David Matheson <david.j.matheson@gmail.com>
2013-04-17 00:29:34 -0700

    LinearMatrixFeature is a linear combination of dimensions (columns) for each sample (row).  Unit tests included.

---
+++ modules/matrix_features/native/LinearMatrixFeature.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "asserts.h"
+#include "VectorBuffer.h"
+#include "MatrixBuffer.h"
+#include "UniqueBufferId.h"
+
+enum
+{
+    MATRIX_FEATURES = 1 // Move this to a more soucefile
+};
+
+const int FEATURE_TYPE_INDEX = 0;     // Move this to a more soucefile
+const int SPLIT_POINT_INDEX = FEATURE_TYPE_INDEX;  // Move this to a more soucefile
+const int NUMBER_OF_DIMENSIONS = FEATURE_TYPE_INDEX + 1;
+const int PARAM_START_INDEX = NUMBER_OF_DIMENSIONS + 1;  // Move this to a more soucefile
+
+// ----------------------------------------------------------------------------
+// 
+// LinearMatrixFeature is a linear combination of dimensions (columns) for
+// each sample (row).
+//
+// ----------------------------------------------------------------------------
+template <class FloatType, class IntType>
+class LinearMatrixFeature
+{
+public:
+    LinearMatrixFeature( const UniqueBufferId::BufferId floatParamsBufferId,
+                    const UniqueBufferId::BufferId intParamsBufferId,
+                    const UniqueBufferId::BufferId indicesBufferId,
+                    const UniqueBufferId::BufferId matrixDataBufferId );
+    ~LinearMatrixFeature();
+
+    void Bind(const BufferCollectionStack& readCollection);
+
+    FloatType FeatureValue( const int featureIndex, const int relativeSampleIndex) const;
+
+private:
+    const UniqueBufferId::BufferId mFloatParamsBufferId;
+    const UniqueBufferId::BufferId mIntParamsBufferId;
+    const UniqueBufferId::BufferId mIndicesBufferId;
+    const UniqueBufferId::BufferId mDataMatrixBufferId;
+
+    MatrixBufferTemplate<FloatType> const* mFloatParams;
+    MatrixBufferTemplate<IntType> const* mIntParams;
+    VectorBufferTemplate<IntType> const* mIndices;
+    MatrixBufferTemplate<FloatType> const* mDataMatrix;
+
+};
+
+template <class FloatType, class IntType>
+LinearMatrixFeature<FloatType, IntType>::LinearMatrixFeature( const UniqueBufferId::BufferId floatParamsBufferId,
+                                                              const UniqueBufferId::BufferId intParamsBufferId,
+                                                              const UniqueBufferId::BufferId indicesBufferId,
+                                                              const UniqueBufferId::BufferId matrixDataBufferId )
+: mFloatParamsBufferId(floatParamsBufferId)
+, mIntParamsBufferId(intParamsBufferId)
+, mIndicesBufferId(indicesBufferId)
+, mDataMatrixBufferId(matrixDataBufferId)
+, mFloatParams(NULL)
+, mIntParams(NULL)
+, mIndices(NULL)
+, mDataMatrix(NULL)
+{}
+
+template <class FloatType, class IntType>
+LinearMatrixFeature<FloatType, IntType>::~LinearMatrixFeature()
+{}
+
+template <class FloatType, class IntType>
+void LinearMatrixFeature<FloatType, IntType>::Bind(const BufferCollectionStack& readCollection)
+{
+    ASSERT(readCollection.HasBuffer< MatrixBufferTemplate<FloatType> >(mFloatParamsBufferId));
+    ASSERT(readCollection.HasBuffer< MatrixBufferTemplate<IntType> >(mIntParamsBufferId));
+    ASSERT(readCollection.HasBuffer< VectorBufferTemplate<IntType> >(mIndicesBufferId));
+    ASSERT(readCollection.HasBuffer< MatrixBufferTemplate<FloatType> >(mDataMatrixBufferId));
+
+    mFloatParams = readCollection.GetBufferPtr< MatrixBufferTemplate<FloatType> >(mFloatParamsBufferId);
+    mIntParams = readCollection.GetBufferPtr< MatrixBufferTemplate<IntType> >(mIntParamsBufferId);
+    mIndices = readCollection.GetBufferPtr< VectorBufferTemplate<IntType> >(mIndicesBufferId);
+    mDataMatrix = readCollection.GetBufferPtr< MatrixBufferTemplate<FloatType> >(mDataMatrixBufferId);
+
+    ASSERT_ARG_DIM_1D(mFloatParams->GetN(), mIntParams->GetN());
+}
+
+template <class FloatType, class IntType>
+FloatType LinearMatrixFeature<FloatType, IntType>::FeatureValue( const int featureIndex, const int relativeSampleIndex) const
+{
+    FloatType featureValue = static_cast<FloatType>(0.0);
+    const IntType numberOfDimensions = mIntParams->Get(featureIndex, NUMBER_OF_DIMENSIONS);
+    for(int i=PARAM_START_INDEX; i<numberOfDimensions + PARAM_START_INDEX; i++)
+    {
+        const IntType dimension = mIntParams->Get(featureIndex, i);
+        const IntType matrixIndex = mIndices->Get(relativeSampleIndex);
+        featureValue += mFloatParams->Get(featureIndex, i) * mDataMatrix->Get(matrixIndex, dimension);
+    }
+    return featureValue;
+}
+
---
+++ modules/matrix_features/test/test_linear_matrix_feature.cpp
@@ -0,0 +1,103 @@
+#include <boost/test/unit_test.hpp>
+
+#include <vector>
+
+#include "VectorBuffer.h"
+#include "MatrixBuffer.h"
+#include "BufferCollection.h"
+#include "BufferCollectionStack.h"
+#include "LinearMatrixFeature.h"
+
+
+template<typename T>
+MatrixBufferTemplate<T> Create4x5ExampleMatrix()
+{
+    T data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    return MatrixBufferTemplate<T>(&data[0], 4, 5);
+}
+
+template<typename T>
+VectorBufferTemplate<T> CreateRange5()
+{
+    T data[] = {0, 1, 2, 3, 4};
+    return VectorBufferTemplate<T>(&data[0], 5);
+}
+
+struct LinearMatrixFeatureFixture {
+
+    LinearMatrixFeatureFixture()
+    : float_params_key("float_params")
+    , int_params_key("int_params")
+    , xs_key("xs")
+    , indices_key("indices")
+    , xs(Create4x5ExampleMatrix<double>())
+    , indices(CreateRange5<int>())
+    , collection()
+    , stack()
+    {
+        collection.AddBuffer(xs_key, xs);
+        collection.AddBuffer(indices_key, indices);
+        stack.Push(&collection);
+    }
+
+    ~LinearMatrixFeatureFixture()
+    {
+    }
+
+    const BufferCollectionKey_t float_params_key;
+    const BufferCollectionKey_t int_params_key;
+    const BufferCollectionKey_t xs_key;
+    const BufferCollectionKey_t indices_key;
+    const MatrixBufferTemplate<double> xs;
+    const VectorBufferTemplate<int> indices;
+    BufferCollection collection;
+    BufferCollectionStack stack;
+};
+
+BOOST_FIXTURE_TEST_SUITE( LinearMatrixFeatureTests,  LinearMatrixFeatureFixture)
+
+BOOST_AUTO_TEST_CASE(test_FeatureValue_axis_aligned)
+{
+    double float_params_data[] = {0, 0, 1,
+                                  0, 0, 1};
+    MatrixBufferTemplate<double> float_params(&float_params_data[0], 2, 3);
+    collection.AddBuffer< MatrixBufferTemplate<double> >(float_params_key, float_params);
+
+    int int_params_data[] = {MATRIX_FEATURES, 1, 0,
+                             MATRIX_FEATURES, 1, 3};
+    MatrixBufferTemplate<int> int_params(&int_params_data[0], 2, 3);
+    collection.AddBuffer< MatrixBufferTemplate<int> >(int_params_key, int_params);
+
+    LinearMatrixFeature<double, int> matrix_feature(  float_params_key, int_params_key, 
+                                                      indices_key, xs_key);
+    matrix_feature.Bind(stack);
+    BOOST_CHECK_EQUAL(matrix_feature.FeatureValue(0, 0), 0);
+    BOOST_CHECK_EQUAL(matrix_feature.FeatureValue(0, 1), 5);
+    BOOST_CHECK_EQUAL(matrix_feature.FeatureValue(0, 2), 10);
+    BOOST_CHECK_EQUAL(matrix_feature.FeatureValue(1, 0), 3);
+    BOOST_CHECK_EQUAL(matrix_feature.FeatureValue(1, 1), 8);
+    BOOST_CHECK_EQUAL(matrix_feature.FeatureValue(1, 2), 13);
+}
+
+BOOST_AUTO_TEST_CASE(test_FeatureValue_linear_combination)
+{
+    double float_params_data[] = {0, 0, -1.0, 0.0, 1.0, 2.0, -3.0,
+                                  0, 0, -0.5, 0.5, 1.5, 1, 1};
+    MatrixBufferTemplate<double> float_params(&float_params_data[0], 2, 7);
+    collection.AddBuffer< MatrixBufferTemplate<double> >(float_params_key, float_params);
+
+    int int_params_data[] = {MATRIX_FEATURES, 5, 0, 1, 2, 3, 4,
+                             MATRIX_FEATURES, 3, 0, 2, 4, 1, 1};
+    MatrixBufferTemplate<int> int_params(&int_params_data[0], 2, 7);
+    collection.AddBuffer< MatrixBufferTemplate<int> >(int_params_key, int_params);
+
+    LinearMatrixFeature<double, int> matrix_feature(  float_params_key, int_params_key, 
+                                                      indices_key, xs_key);
+    matrix_feature.Bind(stack);
+    BOOST_CHECK_EQUAL(matrix_feature.FeatureValue(0, 0), -4);
+    BOOST_CHECK_EQUAL(matrix_feature.FeatureValue(0, 1), -9); 
+    BOOST_CHECK_EQUAL(matrix_feature.FeatureValue(1, 0), 7);
+    BOOST_CHECK_EQUAL(matrix_feature.FeatureValue(1, 1), 14.5);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
