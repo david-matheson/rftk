2b8e88a85941c65742227a68458323c88dd8de6f
David Matheson <david.j.matheson@gmail.com>
2013-04-20 14:53:46 -0700

    BestSplitpointsWalkingSortedStep: Find the split point with the highest impurity for each feature.  It does this by sorting the feature values and walking the sorted values to find the split point with the highest impurity.

---
+++ modules/impurity/native/BestSplitpointsWalkingSortedStep.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include <limits>
+#include <cmath>
+
+#include "VectorBuffer.h"
+#include "MatrixBuffer.h"
+#include "BufferCollection.h"
+#include "FeatureExtractorStep.h"
+#include "PipelineStepI.h"
+#include "UniqueBufferId.h"
+#include "FeatureSorter.h"
+
+// ----------------------------------------------------------------------------
+//
+// Finds the split point with the highest impurity for each feature.  It does
+// this by sorting the feature values and walking the sorted values to find
+// the split point with the highest impurity.
+//
+// ----------------------------------------------------------------------------
+template <class ImpurityWalker>
+class BestSplitpointsWalkingSortedStep : public PipelineStepI
+{
+public:
+    BestSplitpointsWalkingSortedStep (const ImpurityWalker& impurityWalker,
+                              const UniqueBufferId::BufferId& featureValues,
+                              const FeatureValueOrdering featureValueOrdering );
+    virtual ~BestSplitpointsWalkingSortedStep();
+
+    virtual PipelineStepI* Clone() const;
+
+    virtual void ProcessStep(   const BufferCollectionStack& readCollection,
+                                BufferCollection& writeCollection) const;
+
+    // Read only output buffer
+    const UniqueBufferId::BufferId ImpurityBufferId;
+    const UniqueBufferId::BufferId ThresholdBufferId;
+    const UniqueBufferId::BufferId ThresholdCountsBufferId;
+    const UniqueBufferId::BufferId ChildCountsBufferId;
+    const UniqueBufferId::BufferId LeftYsBufferId;
+    const UniqueBufferId::BufferId RightYsBufferId;
+private:
+    const ImpurityWalker mImpurityWalker;
+    const UniqueBufferId::BufferId mFeatureValuesBufferId;
+    const FeatureValueOrdering mFeatureValueOrdering;
+
+};
+
+
+template <class ImpurityWalker>
+BestSplitpointsWalkingSortedStep<ImpurityWalker>::BestSplitpointsWalkingSortedStep(const ImpurityWalker& impurityWalker,
+                                                                      const UniqueBufferId::BufferId& featureValues,
+                                                                      const FeatureValueOrdering featureValueOrdering )
+: ImpurityBufferId( UniqueBufferId::GetBufferId("Impurity") )
+, ThresholdBufferId( UniqueBufferId::GetBufferId("Threshold") )
+, ThresholdCountsBufferId( UniqueBufferId::GetBufferId("ThresholdCounts") )
+, ChildCountsBufferId( UniqueBufferId::GetBufferId("ChildCounts") )
+, LeftYsBufferId( UniqueBufferId::GetBufferId("LeftYs") )
+, RightYsBufferId( UniqueBufferId::GetBufferId("RightYs") )
+, mImpurityWalker(impurityWalker)
+, mFeatureValuesBufferId(featureValues)
+, mFeatureValueOrdering(featureValueOrdering)
+{}
+
+template <class ImpurityWalker>
+BestSplitpointsWalkingSortedStep<ImpurityWalker>::~BestSplitpointsWalkingSortedStep()
+{}
+
+template <class ImpurityWalker>
+PipelineStepI* BestSplitpointsWalkingSortedStep<ImpurityWalker>::Clone() const
+{
+    BestSplitpointsWalkingSortedStep* clone = new BestSplitpointsWalkingSortedStep<ImpurityWalker>(*this);
+    return clone;
+}
+
+template <class ImpurityWalker>
+void BestSplitpointsWalkingSortedStep<ImpurityWalker>::ProcessStep(const BufferCollectionStack& readCollection,
+                                                              BufferCollection& writeCollection) const
+{
+    // Bind input buffers
+    ASSERT(readCollection.HasBuffer< MatrixBufferTemplate<typename ImpurityWalker::Float> >(mFeatureValuesBufferId));
+    MatrixBufferTemplate<typename ImpurityWalker::Float> const& featureValues
+           = readCollection.GetBuffer< MatrixBufferTemplate<typename ImpurityWalker::Float> >(mFeatureValuesBufferId);
+
+    // Make a local non-const walker and bind it
+    ImpurityWalker impurityWalker = mImpurityWalker;
+    impurityWalker.Bind(readCollection);
+    const int numberOfFeatures =  mFeatureValueOrdering == FEATURES_BY_DATAPOINTS ? featureValues.GetM() : featureValues.GetN();
+
+    // Bind output buffers
+    const int numberOfThresholds = 1;
+    MatrixBufferTemplate<typename ImpurityWalker::Float>& impurities
+           = writeCollection.GetOrAddBuffer< MatrixBufferTemplate<typename ImpurityWalker::Float> >(ImpurityBufferId);
+    impurities.Resize(numberOfFeatures,1);
+
+    MatrixBufferTemplate<typename ImpurityWalker::Float>& thresholds
+           = writeCollection.GetOrAddBuffer< MatrixBufferTemplate<typename ImpurityWalker::Float> >(ThresholdBufferId);
+    thresholds.Resize(numberOfFeatures,1);
+
+    VectorBufferTemplate<typename ImpurityWalker::Int>& thresholdCounts
+           = writeCollection.GetOrAddBuffer< VectorBufferTemplate<typename ImpurityWalker::Int> >(ThresholdCountsBufferId);
+    thresholdCounts.Resize(numberOfFeatures);
+
+    Tensor3BufferTemplate<typename ImpurityWalker::Float>& childCounts
+           = writeCollection.GetOrAddBuffer< Tensor3BufferTemplate<typename ImpurityWalker::Float> >(ChildCountsBufferId);
+    childCounts.Resize(numberOfFeatures, 1, 2);
+
+    Tensor3BufferTemplate<typename ImpurityWalker::Float>& leftYs
+           = writeCollection.GetOrAddBuffer< Tensor3BufferTemplate<typename ImpurityWalker::Float> >(LeftYsBufferId);
+    leftYs.Resize(numberOfFeatures, 1, impurityWalker.GetYDim());
+
+    Tensor3BufferTemplate<typename ImpurityWalker::Float>& rightYs
+           = writeCollection.GetOrAddBuffer< Tensor3BufferTemplate<typename ImpurityWalker::Float> >(RightYsBufferId);
+    rightYs.Resize(numberOfFeatures, 1, impurityWalker.GetYDim());
+
+    for(int f=0; f<numberOfFeatures; f++)
+    {
+        impurityWalker.Reset();
+
+        typename ImpurityWalker::Float bestImpurity = std::numeric_limits<typename ImpurityWalker::Float>::min();
+        typename ImpurityWalker::Float bestThreshold = std::numeric_limits<typename ImpurityWalker::Float>::min();
+        typename ImpurityWalker::Float bestLeftChildCounts = typename ImpurityWalker::Float(0);
+        typename ImpurityWalker::Float bestRightChildCounts = typename ImpurityWalker::Float(0);
+        VectorBufferTemplate<typename ImpurityWalker::Float> bestLeftYs(impurityWalker.GetYDim());
+        VectorBufferTemplate<typename ImpurityWalker::Float> bestRightYs(impurityWalker.GetYDim());
+
+        FeatureSorter<typename ImpurityWalker::Float> sorter(featureValues, mFeatureValueOrdering, f);
+        sorter.Sort();
+        for(int sortedIndex=0; sortedIndex<sorter.GetNumberOfSamples()-1; sortedIndex++)
+        {
+            const int i = sorter.GetUnSortedIndex(sortedIndex);
+
+            impurityWalker.MoveLeftToRight(i);
+
+            const typename ImpurityWalker::Float consecutiveFeatureDelta = sorter.GetFeatureValue(sortedIndex+1) - sorter.GetFeatureValue(sortedIndex);
+            
+            if((std::abs(consecutiveFeatureDelta) > std::numeric_limits<typename ImpurityWalker::Float>::epsilon())
+              && impurityWalker.Impurity() > bestImpurity)
+            {
+                bestImpurity = impurityWalker.Impurity();
+                bestThreshold = sorter.GetFeatureValue(sortedIndex) + 0.5*consecutiveFeatureDelta;
+                bestLeftChildCounts = impurityWalker.GetLeftChildCounts();
+                bestRightChildCounts = impurityWalker.GetRightChildCounts();
+                bestLeftYs = impurityWalker.GetLeftYs();
+                bestRightYs = impurityWalker.GetRightYs();
+            }
+        }
+
+        impurities.Set(f, 0, bestImpurity);
+        thresholds.Set(f, 0, bestThreshold);
+        thresholdCounts.Set(f, 1);
+        childCounts.Set(f, 0, 0, bestLeftChildCounts);
+        childCounts.Set(f, 0, 1, bestRightChildCounts);
+        leftYs.SetRow(f, 0, bestLeftYs );
+        rightYs.SetRow(f, 0, bestRightYs );
+    }
+}
---
+++ modules/impurity/test/test_best_splitpoints_walking_sorted_step.cpp
@@ -0,0 +1,215 @@
+#include <boost/test/unit_test.hpp>
+
+#include "VectorBuffer.h"
+#include "MatrixBuffer.h"
+#include "BufferCollection.h"
+#include "BufferCollectionStack.h"
+#include "BestSplitpointsWalkingSortedStep.h"
+#include "TestBufferWalker.h"
+
+template<typename T>
+VectorBufferTemplate<T> CreateVector(T data[], int n)
+{
+    return VectorBufferTemplate<T>(&data[0], n);
+}
+
+template<typename T>
+MatrixBufferTemplate<T> CreateMatrix(T data[], int m, int n)
+{
+    return MatrixBufferTemplate<T>(&data[0], m, n);
+}
+
+template<typename T>
+Tensor3BufferTemplate<T> CreateTensor3(T data[], int l, int m, int n)
+{
+    return Tensor3BufferTemplate<T>(&data[0], l, m, n);
+}
+
+template<typename T>
+MatrixBufferTemplate<T> CreateFeatureMatrix()
+{
+    T data[] = {0.06, 0.03, 0.07, 0,    0.02,
+                6.1,  3,    5.5,  8.9,  2, 
+                9,    3,    6.25, 8.75, 8.8};
+
+    return MatrixBufferTemplate<T>(&data[0], 3, 5);
+}
+
+template<typename T>
+MatrixBufferTemplate<T> CreateImpurityMatrix()
+{
+    T data[] = {0.01, 0.02, 0.05, 0.2, 0.04,
+                50, 8, 6, 4, 2, 
+                20, 10, 30, 20, 40,
+                7, 4, 66, 33, 2,
+                55, 6, -8, 2, 3};
+
+    return MatrixBufferTemplate<T>(&data[0], 5, 5);
+}
+
+template<typename T>
+MatrixBufferTemplate<T> CreateLeftYsMatrix()
+{
+    T data[] = {1, 2, 3, 4, 5,
+                10, 8, 6, 4, 2, 
+                20, 10, 30, 20, 40,
+                7, 4, 66, 33, 2,
+                55, 6, -8, 2, 3};
+
+    return MatrixBufferTemplate<T>(&data[0], 5, 5);
+}
+
+template<typename T>
+MatrixBufferTemplate<T> CreateRightYsMatrix()
+{
+    T data[] = {10, 8, 6, 4, 2, 
+                20, 10, 30, 20, 40,
+                7, 4, 66, 33, 2,
+                55, 6, -8, 2, 3,
+                1, 2, 3, 4, 5,};
+
+    return MatrixBufferTemplate<T>(&data[0], 5, 5);
+}
+
+struct BestSplitpointsWalkingSortedStepFixture {
+    BestSplitpointsWalkingSortedStepFixture()
+    : fm_key("feature")
+    , fm( CreateFeatureMatrix<double>() )
+    , im( CreateImpurityMatrix<double>() )
+    , left( CreateLeftYsMatrix<double>() )
+    , right( CreateRightYsMatrix<double>() )
+    , collection()
+    , stack()
+    {
+        collection.AddBuffer(fm_key, fm);
+        stack.Push(&collection);
+    }
+
+    ~BestSplitpointsWalkingSortedStepFixture()
+    {
+    }
+
+    const BufferCollectionKey_t fm_key;
+    const MatrixBufferTemplate<double> fm;
+    const MatrixBufferTemplate<double> im;    
+    const MatrixBufferTemplate<double> left;
+    const MatrixBufferTemplate<double> right;
+    BufferCollection collection;
+    BufferCollectionStack stack;
+};
+
+BOOST_FIXTURE_TEST_SUITE( BestSplitpointsWalkingSortedStepTests,  BestSplitpointsWalkingSortedStepFixture )
+
+
+BOOST_AUTO_TEST_CASE(test_BestSplitpointsWalkingSortedStep_ProcessStep_FEATURES_BY_DATAPOINTS)
+{
+    BOOST_CHECK( fm == stack.GetBuffer< MatrixBufferTemplate<double> >(fm_key) );
+    TestBufferWalker<double, int> walker(im, left, right);
+    BestSplitpointsWalkingSortedStep< TestBufferWalker<double, int> > bestsplits(walker, fm_key, FEATURES_BY_DATAPOINTS);
+    bestsplits.ProcessStep(stack, collection);
+
+    BOOST_CHECK( collection.HasBuffer< MatrixBufferTemplate<double> >( bestsplits.ImpurityBufferId ) );
+    MatrixBufferTemplate<double>& best_impurity = collection.GetBuffer< MatrixBufferTemplate<double> >( bestsplits.ImpurityBufferId );
+    double expected_best_impurity_data[] = {0.2, 50, 40};
+    MatrixBufferTemplate<double> expected_best_impurity = CreateMatrix<double>(expected_best_impurity_data, 3, 1);
+    BOOST_CHECK(best_impurity == expected_best_impurity);
+
+    BOOST_CHECK( collection.HasBuffer< MatrixBufferTemplate<double> >( bestsplits.ThresholdBufferId ) );
+    MatrixBufferTemplate<double>& best_thresholds = collection.GetBuffer< MatrixBufferTemplate<double> >( bestsplits.ThresholdBufferId );
+    double expected_best_thresholds_data[] = {0.01, 7.5, 8.9};
+    MatrixBufferTemplate<double> expected_best_thresholds = CreateMatrix<double>(expected_best_thresholds_data, 3, 1);
+    BOOST_CHECK(best_thresholds == expected_best_thresholds);
+
+    BOOST_CHECK( collection.HasBuffer< VectorBufferTemplate<int> >( bestsplits.ThresholdCountsBufferId ) );
+    VectorBufferTemplate<int>& threshold_counts = collection.GetBuffer< VectorBufferTemplate<int> >( bestsplits.ThresholdCountsBufferId );
+    int expected_threshold_counts_data[] = {1, 1, 1};
+    VectorBufferTemplate<int> expected_threshold_counts = CreateVector<int>(expected_threshold_counts_data, 3);
+    BOOST_CHECK(threshold_counts == expected_threshold_counts);
+
+    BOOST_CHECK( collection.HasBuffer< Tensor3BufferTemplate<double> >( bestsplits.ChildCountsBufferId ) );
+    Tensor3BufferTemplate<double>& child_counts = collection.GetBuffer< Tensor3BufferTemplate<double> >( bestsplits.ChildCountsBufferId );
+    double expected_child_counts_data[] = {15, 30,
+                                          30, 120,
+                                          120, 112};
+    Tensor3BufferTemplate<double> expected_child_counts = CreateTensor3<double>(expected_child_counts_data, 3, 1, 2);
+    BOOST_CHECK(child_counts == expected_child_counts);
+
+    BOOST_CHECK( collection.HasBuffer< Tensor3BufferTemplate<double> >( bestsplits.LeftYsBufferId ) );
+    Tensor3BufferTemplate<double>& left_ys = collection.GetBuffer< Tensor3BufferTemplate<double> >( bestsplits.LeftYsBufferId );
+    double expected_left_ys_data[] = {1, 2, 3, 4, 5,
+                                      10, 8, 6, 4, 2, 
+                                      20, 10, 30, 20, 40};
+    Tensor3BufferTemplate<double> expected_left_ys = CreateTensor3<double>(expected_left_ys_data, 3, 1, 5);
+    BOOST_CHECK(left_ys == expected_left_ys);
+
+    BOOST_CHECK( collection.HasBuffer< Tensor3BufferTemplate<double> >( bestsplits.RightYsBufferId ) );
+    Tensor3BufferTemplate<double>& right_ys = collection.GetBuffer< Tensor3BufferTemplate<double> >( bestsplits.RightYsBufferId );
+    double expected_right_ys_data[] = {10, 8, 6, 4, 2, 
+                                      20, 10, 30, 20, 40,
+                                      7, 4, 66, 33, 2};
+    Tensor3BufferTemplate<double> expected_right_ys = CreateTensor3<double>(expected_right_ys_data, 3, 1, 5);
+    BOOST_CHECK(right_ys == expected_right_ys);
+}
+
+BOOST_AUTO_TEST_CASE(test_BestSplitpointsWalkingSortedStep_ProcessStep_DATAPOINTS_BY_FEATURES)
+{
+    BOOST_CHECK( fm == stack.GetBuffer< MatrixBufferTemplate<double> >(fm_key) );
+    TestBufferWalker<double, int> walker(im, left, right);
+    BestSplitpointsWalkingSortedStep< TestBufferWalker<double, int> > bestsplits(walker, fm_key, DATAPOINTS_BY_FEATURES);
+    bestsplits.ProcessStep(stack, collection);
+
+    BOOST_CHECK( collection.HasBuffer< MatrixBufferTemplate<double> >( bestsplits.ImpurityBufferId ) );
+    MatrixBufferTemplate<double>& best_impurity = collection.GetBuffer< MatrixBufferTemplate<double> >( bestsplits.ImpurityBufferId );
+    double expected_best_impurity_data[] = {0.02, 50, 20, 66, 55};
+    MatrixBufferTemplate<double> expected_best_impurity = CreateMatrix<double>(expected_best_impurity_data, 5, 1);
+    BOOST_CHECK(best_impurity == expected_best_impurity);
+
+    BOOST_CHECK( collection.HasBuffer< MatrixBufferTemplate<double> >( bestsplits.ThresholdBufferId ) );
+    MatrixBufferTemplate<double>& best_thresholds = collection.GetBuffer< MatrixBufferTemplate<double> >( bestsplits.ThresholdBufferId );
+    double expected_best_thresholds_data[] = {7.55, 1.515, 2.785, 8.825, 1.01};
+    MatrixBufferTemplate<double> expected_best_thresholds = CreateMatrix<double>(expected_best_thresholds_data, 5, 1);
+    BOOST_CHECK_CLOSE(best_thresholds.Get(0,0), expected_best_thresholds.Get(0,0), 0.001);
+    BOOST_CHECK_CLOSE(best_thresholds.Get(1,0), expected_best_thresholds.Get(1,0), 0.001);
+    BOOST_CHECK_CLOSE(best_thresholds.Get(2,0), expected_best_thresholds.Get(2,0), 0.001);
+    BOOST_CHECK_CLOSE(best_thresholds.Get(3,0), expected_best_thresholds.Get(3,0), 0.001);
+    BOOST_CHECK_CLOSE(best_thresholds.Get(4,0), expected_best_thresholds.Get(4,0), 0.001);
+    // BOOST_CHECK(best_thresholds == expected_best_thresholds);
+
+    BOOST_CHECK( collection.HasBuffer< VectorBufferTemplate<int> >( bestsplits.ThresholdCountsBufferId ) );
+    VectorBufferTemplate<int>& threshold_counts = collection.GetBuffer< VectorBufferTemplate<int> >( bestsplits.ThresholdCountsBufferId );
+    int expected_threshold_counts_data[] = {1, 1, 1, 1, 1};
+    VectorBufferTemplate<int> expected_threshold_counts = CreateVector<int>(expected_threshold_counts_data, 5);
+    BOOST_CHECK(threshold_counts == expected_threshold_counts);
+
+    BOOST_CHECK( collection.HasBuffer< Tensor3BufferTemplate<double> >( bestsplits.ChildCountsBufferId ) );
+    Tensor3BufferTemplate<double>& child_counts = collection.GetBuffer< Tensor3BufferTemplate<double> >( bestsplits.ChildCountsBufferId );
+    double expected_child_counts_data[] = {15, 30,
+                                          30, 120,
+                                          120, 112,
+                                          112, 58,
+                                          58, 15};
+    Tensor3BufferTemplate<double> expected_child_counts = CreateTensor3<double>(expected_child_counts_data, 5, 1, 2);
+    BOOST_CHECK(child_counts == expected_child_counts);
+
+    BOOST_CHECK( collection.HasBuffer< Tensor3BufferTemplate<double> >( bestsplits.LeftYsBufferId ) );
+    Tensor3BufferTemplate<double>& left_ys = collection.GetBuffer< Tensor3BufferTemplate<double> >( bestsplits.LeftYsBufferId );
+    double expected_left_ys_data[] = {1, 2, 3, 4, 5,
+                                      10, 8, 6, 4, 2, 
+                                      20, 10, 30, 20, 40,
+                                      7, 4, 66, 33, 2,
+                                      55, 6, -8, 2, 3};
+    Tensor3BufferTemplate<double> expected_left_ys = CreateTensor3<double>(expected_left_ys_data, 5, 1, 5); 
+    BOOST_CHECK(left_ys == expected_left_ys);
+
+    BOOST_CHECK( collection.HasBuffer< Tensor3BufferTemplate<double> >( bestsplits.RightYsBufferId ) );
+    Tensor3BufferTemplate<double>& right_ys = collection.GetBuffer< Tensor3BufferTemplate<double> >( bestsplits.RightYsBufferId );
+    double expected_right_ys_data[] = {10, 8, 6, 4, 2, 
+                                      20, 10, 30, 20, 40,
+                                      7, 4, 66, 33, 2,
+                                      55, 6, -8, 2, 3,
+                                      1, 2, 3, 4, 5};
+    Tensor3BufferTemplate<double> expected_right_ys = CreateTensor3<double>(expected_right_ys_data, 5, 1, 5);
+    BOOST_CHECK(right_ys == expected_right_ys);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
