314042772f896e4a781bd10de0a43ffa50abd1d5
David Matheson <david.j.matheson@gmail.com>
2013-04-22 13:41:53 -0700

    TrySplit: Add TrySplitNoCriteria which will always try to split a node

---
+++ modules/try_split/native/TrySplitNoCriteria.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "TrySplitCriteriaI.h"
+
+// ----------------------------------------------------------------------------
+//
+// TrySplitNoCriteria has no criteria and always tries to split the node
+//
+// ----------------------------------------------------------------------------
+class TrySplitNoCriteria: public TrySplitCriteriaI
+{
+public:
+    TrySplitNoCriteria();
+    virtual ~TrySplitNoCriteria();
+
+    virtual TrySplitCriteriaI* Clone() const;
+
+    virtual bool TrySplit(int depth, int numberOfDatapoints) const;
+};
