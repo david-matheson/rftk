458c0b79ef12695155d8bd3741a0bd761dad8833
David Matheson <david.j.matheson@gmail.com>
2013-05-01 02:39:04 -0700

    Vanilia classifier tests and experiment runner for icml

---
+++ icml_experiments/train_vanilia_offline.py
@@ -0,0 +1,69 @@
+import numpy as np
+import argparse
+import itertools
+import cPickle as pickle
+from datetime import datetime
+import matplotlib.pyplot as plt
+import rftk
+
+import experiment_measurement as exp_measurement
+
+
+def run_experiment(config):
+    data_config = config.get_data_config()
+
+    out_measurements_filename = ("experiment_data/%s-new-offline-measurements-%s.pkl") % (
+                          config.get_experiment_name(),
+                          str(datetime.now()).replace(':', '-').replace(' ', '-'))
+
+    measurements = []
+    for number_of_samples, number_of_passes in itertools.product(data_config.data_sizes, data_config.number_of_passes_through_data):
+        X_train, Y_train, X_test, Y_test = data_config.load_data(number_of_samples, number_of_passes)
+
+
+        for run_id in range(data_config.number_of_runs):
+
+            print "Starting training", str(datetime.now())
+            sklearn_config = config.get_sklearn_offline_config()
+            learner = rftk.learn.create_vanilia_classifier()
+            predictor = learner.fit(x=X_train, classes=Y_train, 
+                                    number_of_features=sklearn_config.number_of_features, 
+                                    number_of_trees=sklearn_config.number_of_trees,
+                                    max_depth=sklearn_config.max_depth,
+                                    min_node_size=sklearn_config.min_samples_split,
+                                    # min_child_size=sklearn_config.min_samples_leaf,
+                                    number_of_jobs=sklearn_config.number_of_jobs)
+            #todo add max_depth, min_samples_split, min_samples_leaf, 
+            print "Starting testing", str(datetime.now())
+
+            y_probs = predictor.predict(x=X_test)
+            y_hat = y_probs.argmax(axis=1)
+            accurracy = np.mean(Y_test == y_hat)
+            print "%d %d %d: %0.5f" % (number_of_samples, number_of_passes, run_id, accurracy)
+            forest_measurement = exp_measurement.SklearnForestMeasurement(data_config, sklearn_config,
+                number_of_samples, number_of_passes, accurracy)
+            measurements.append(forest_measurement)
+
+            # (m,n) = X_test.shape
+            # for i in range(m):
+            #     if( Y_test[i] != y_hat[i] ):
+            #         print Y_test[i] -1
+            #         print y_hat[i] -1
+            #         failed_img = X_test[i,:].reshape(16,16)
+            #         plt.imshow(failed_img)
+            #         plt.show()
+
+
+        pickle.dump(measurements, open(out_measurements_filename, "wb"))
+
+    return measurements
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Sklearn accuracy on data')
+    parser.add_argument('-c', '--config_file', help='experiment config file', required=True)
+    args = parser.parse_args()
+
+    config = __import__(args.config_file)
+
+    measurements = run_experiment(config)
---
+++ tests/test_vanilia_classifier.py
@@ -0,0 +1,27 @@
+import unittest as unittest
+import numpy as np
+
+import rftk
+import rftk.buffers as buffers
+import rftk.bootstrap as bootstrap
+import rftk.pipeline as pipeline
+import rftk.matrix_features as matrix_features
+import rftk.classification as classification
+import rftk.try_split as try_split
+import rftk.splitpoints as splitpoints
+import rftk.should_split as should_split
+import rftk.forest_data as forest_data
+import rftk.learn as learn
+import rftk.predict as predict
+
+class TestNew(unittest.TestCase):
+
+    def test_vanilia_classifier(self):
+        learner = rftk.learn.create_vanilia_classifier()
+        x = np.array([[3,1],[3,2], [3,3], [0,1], [0,2]], dtype=np.float32)
+        classes = np.array([0,0,0,1,2], dtype=np.int32)
+        predictor = learner.fit(x=x, classes=classes, number_of_features=2, bootstrap=False)
+        result = predictor.predict(x=x)
+
+if __name__ == '__main__':
+    unittest.main()
