3224c1819479b160b6e0674a32d689a9a5e8707c
David Matheson <david.j.matheson@gmail.com>
2013-05-14 15:33:21 -0700

    Factory for one stream online learner

--- modules/learn/python/matrix_learner.py
+++ modules/learn/python/matrix_learner.py
@@ -244,3 +244,100 @@ def create_two_stream_classifier():
                             create_axis_aligned_matrix_two_stream_learner_32f,
                             create_matrix_predictor_32f)
 
+def create_online_axis_aligned_matrix_one_stream_learner_32f(**kwargs):
+    number_of_trees = int( kwargs.get('number_of_trees', 10) )
+    number_of_features = int( kwargs.get('number_of_features', np.sqrt(kwargs['x'].shape[1])) )
+    feature_ordering = int( kwargs.get('feature_ordering', pipeline.FEATURES_BY_DATAPOINTS) )
+    number_of_splitpoints = int( kwargs.get('number_of_splitpoints', 1 ))
+    number_of_classes = int( np.max(kwargs['classes']) + 1 )
+    max_frontier_size = int( kwargs.get('max_frontier_size', 10000000) )
+    impurity_update_period = int( kwargs.get('impurity_update_period', 1) )
+
+    try_split_criteria = create_try_split_criteria(**kwargs)
+
+    if 'bootstrap' in kwargs and kwargs.get('bootstrap'):
+        print 'bootstrap'
+        sample_data_step = pipeline.BootstrapSamplesStep_f32f32i32(buffers.X_FLOAT_DATA)
+    elif 'poisson_sample' in kwargs:
+        poisson_sample_mean = float(kwargs.get('poisson_sample'))
+        print 'poisson_sample %f' % poisson_sample_mean
+        sample_data_step = pipeline.PoissonSamplesStep_f32i32(buffers.X_FLOAT_DATA, poisson_sample_mean)
+    else:
+        print 'sample_all'
+        sample_data_step = pipeline.AllSamplesStep_f32f32i32(buffers.X_FLOAT_DATA)
+
+    number_of_features_buffer = buffers.as_vector_buffer(np.array([number_of_features], dtype=np.int32))
+    set_number_features_step = pipeline.SetInt32VectorBufferStep(number_of_features_buffer, pipeline.WHEN_NEW)
+    tree_steps_pipeline = pipeline.Pipeline([sample_data_step, set_number_features_step])
+
+    feature_params_step = matrix_features.AxisAlignedParamsStep_f32i32(set_number_features_step.OutputBufferId, buffers.X_FLOAT_DATA)
+
+    matrix_feature = matrix_features.LinearFloat32MatrixFeature_f32i32(feature_params_step.FloatParamsBufferId,
+                                                                      feature_params_step.IntParamsBufferId,
+                                                                      sample_data_step.IndicesBufferId,
+                                                                      buffers.X_FLOAT_DATA)
+    matrix_feature_extractor_step = matrix_features.LinearFloat32MatrixFeatureExtractorStep_f32i32(matrix_feature, feature_ordering)
+    slice_classes_step = pipeline.SliceInt32VectorBufferStep_i32(buffers.CLASS_LABELS, sample_data_step.IndicesBufferId)
+    slice_weights_step = pipeline.SliceFloat32VectorBufferStep_i32(sample_data_step.WeightsBufferId, sample_data_step.IndicesBufferId)
+
+    random_splitpoint_selection_step = splitpoints.RandomSplitpointsStep_f32i32(matrix_feature_extractor_step.FeatureValuesBufferId,
+                                                                                number_of_splitpoints,
+                                                                                feature_ordering)
+
+
+    class_stats_updater = classification.ClassStatsUpdater_f32i32(slice_weights_step.SlicedBufferId,
+                                                                      slice_classes_step.SlicedBufferId,
+                                                                      number_of_classes)
+    one_stream_split_stats_step = classification.ClassStatsUpdaterOneStreamStep_f32i32(random_splitpoint_selection_step.SplitpointsBufferId,
+                                                                          random_splitpoint_selection_step.SplitpointsCountsBufferId,
+                                                                          matrix_feature_extractor_step.FeatureValuesBufferId,
+                                                                          feature_ordering,
+                                                                          class_stats_updater)
+
+    impurity_step = classification.ClassInfoGainSplitpointsImpurity_f32i32(random_splitpoint_selection_step.SplitpointsCountsBufferId,
+                                                                          one_stream_split_stats_step.ChildCountsBufferId,
+                                                                          one_stream_split_stats_step.LeftStatsBufferId,
+                                                                          one_stream_split_stats_step.RightStatsBufferId)
+
+    init_node_steps_pipeline = pipeline.Pipeline([feature_params_step])
+    update_stats_node_steps_pipeline = pipeline.Pipeline([matrix_feature_extractor_step,
+                                                          slice_classes_step, 
+                                                          slice_weights_step,
+                                                          random_splitpoint_selection_step,
+                                                          one_stream_split_stats_step])
+    update_impurity_node_steps_pipeline = pipeline.Pipeline([impurity_step])
+
+    split_buffers = splitpoints.SplitSelectorBuffers(impurity_step.ImpurityBufferId,
+                                                          random_splitpoint_selection_step.SplitpointsBufferId,
+                                                          random_splitpoint_selection_step.SplitpointsCountsBufferId,
+                                                          one_stream_split_stats_step.ChildCountsBufferId,
+                                                          one_stream_split_stats_step.LeftStatsBufferId,
+                                                          one_stream_split_stats_step.RightStatsBufferId,
+                                                          feature_params_step.FloatParamsBufferId,
+                                                          feature_params_step.IntParamsBufferId,
+                                                          matrix_feature_extractor_step.FeatureValuesBufferId,
+                                                          feature_ordering,
+                                                          sample_data_step.IndicesBufferId)
+    should_split_criteria = create_should_split_criteria(**kwargs)
+    finalizer = classification.ClassEstimatorFinalizer_f32()
+    split_selector = splitpoints.SplitSelector_f32i32([split_buffers], should_split_criteria, finalizer )
+
+    matrix_feature_prediction = matrix_features.LinearFloat32MatrixFeature_f32i32(sample_data_step.IndicesBufferId,
+                                                                        buffers.X_FLOAT_DATA)
+    estimator_params_updater = classification.ClassEstimatorUpdater_f32i32(sample_data_step.WeightsBufferId, buffers.CLASS_LABELS, number_of_classes)
+    forest_learner = learn.OnlineForestMatrixClassLearner_f32i32(
+                                                try_split_criteria, 
+                                                tree_steps_pipeline, 
+                                                init_node_steps_pipeline, 
+                                                update_stats_node_steps_pipeline,
+                                                update_impurity_node_steps_pipeline,
+                                                impurity_update_period, split_selector,
+                                                max_frontier_size, number_of_trees, 5, 5, number_of_classes,
+                                                sample_data_step.IndicesBufferId, sample_data_step.WeightsBufferId,
+                                                matrix_feature_prediction, estimator_params_updater)
+    return forest_learner
+
+def create_online_one_stream_classifier():
+    return LearnerWrapper(  matrix_classification_data_prepare,
+                            create_online_axis_aligned_matrix_one_stream_learner_32f,
+                            create_matrix_predictor_32f)
