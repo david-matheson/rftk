623876ab2b1ff4dc0fccc893faa349414be89846
David Matheson <david.j.matheson@gmail.com>
2013-05-23 23:19:07 -0700

    Kinect offset regression using Biau 2008

--- modules/learn/python/regression_depth_delta_learner.py
+++ modules/learn/python/regression_depth_delta_learner.py
@@ -97,6 +97,73 @@ def create_regression_scaled_depth_delta_learner_32f(**kwargs):
     forest_learner = learn.ParallelForestLearner(tree_learner, number_of_trees, 5, 5, dimension_of_y*2, number_of_jobs)
     return forest_learner
 
+
+def create_biau2008_regression_scaled_depth_delta_learner_32f(**kwargs):
+    ux = float( kwargs.get('ux') )
+    uy = float( kwargs.get('uy') )
+    vx = float( kwargs.get('vx') )
+    vy = float( kwargs.get('vy') )
+
+    number_of_trees = int( kwargs.get('number_of_trees', 10) )
+    number_of_leaves = int( kwargs.get('number_of_leaves', 10) )
+    number_of_features = 1
+    feature_ordering = int( kwargs.get('feature_ordering', pipeline.FEATURES_BY_DATAPOINTS) )
+    number_of_jobs = int( kwargs.get('number_of_jobs', 1) )
+    dimension_of_y = int( kwargs['y'].GetN() )
+
+    try_split_criteria = try_split.MinNodeSizeCriteria(2)
+
+    if 'bootstrap' in kwargs and kwargs.get('bootstrap'):
+        sample_data_step = pipeline.BootstrapSamplesStep_i32f32i32(buffers.PIXEL_INDICES)
+    else:
+        sample_data_step = pipeline.AllSamplesStep_i32f32i32(buffers.PIXEL_INDICES)
+
+    number_of_features_buffer = buffers.as_vector_buffer(np.array([number_of_features], dtype=np.int32))
+    set_number_features_step = pipeline.SetInt32VectorBufferStep(number_of_features_buffer, pipeline.WHEN_NEW)
+    tree_steps_pipeline = pipeline.Pipeline([sample_data_step, set_number_features_step])
+
+    feature_params_step = image_features.PixelPairGaussianOffsetsStep_f32i32(set_number_features_step.OutputBufferId, ux, uy, vx, vy )
+    depth_delta_feature = image_features.ScaledDepthDeltaFeature_f32i32(feature_params_step.FloatParamsBufferId,
+                                                                      feature_params_step.IntParamsBufferId,
+                                                                      sample_data_step.IndicesBufferId,
+                                                                      buffers.PIXEL_INDICES,
+                                                                      buffers.DEPTH_IMAGES,
+                                                                      buffers.OFFSET_SCALES)
+    depth_delta_feature_extractor_step = image_features.ScaledDepthDeltaFeatureExtractorStep_f32i32(depth_delta_feature, feature_ordering)
+    slice_ys_step = pipeline.SliceFloat32MatrixBufferStep_i32(buffers.YS, sample_data_step.IndicesBufferId)
+    slice_weights_step = pipeline.SliceFloat32VectorBufferStep_i32(sample_data_step.WeightsBufferId, sample_data_step.IndicesBufferId)
+
+    impurity_walker = regression.SumOfVarianceWalker_f32i32(slice_weights_step.SlicedBufferId,
+                                                            slice_ys_step.SlicedBufferId,
+                                                            dimension_of_y)
+
+    best_splitpint_step = regression.SumOfVarianceRandomGapSplitpointsStep_f32i32(impurity_walker,
+                                                                depth_delta_feature_extractor_step.FeatureValuesBufferId,
+                                                                feature_ordering)
+
+    node_steps_pipeline = pipeline.Pipeline([feature_params_step, depth_delta_feature_extractor_step,
+                                            slice_ys_step, slice_weights_step, best_splitpint_step])
+
+    split_buffers = splitpoints.SplitSelectorBuffers(best_splitpint_step.ImpurityBufferId,
+                                                          best_splitpint_step.SplitpointBufferId,
+                                                          best_splitpint_step.SplitpointCountsBufferId,
+                                                          best_splitpint_step.ChildCountsBufferId,
+                                                          best_splitpint_step.LeftYsBufferId,
+                                                          best_splitpint_step.RightYsBufferId,
+                                                          feature_params_step.FloatParamsBufferId,
+                                                          feature_params_step.IntParamsBufferId,
+                                                          depth_delta_feature_extractor_step.FeatureValuesBufferId,
+                                                          feature_ordering,
+                                                          sample_data_step.IndicesBufferId)
+    should_split_criteria = min_child_size_criteria = should_split.MinChildSizeCriteria(1)
+    finalizer = regression.MeanVarianceEstimatorFinalizer_f32()
+    split_selector = splitpoints.SplitSelector_f32i32([split_buffers], should_split_criteria, finalizer )
+
+    tree_learner = learn.Biau2008TreeLearner_f32i32(try_split_criteria, tree_steps_pipeline, node_steps_pipeline, split_selector, number_of_leaves)
+    forest_learner = learn.ParallelForestLearner(tree_learner, number_of_trees, 5, 5, dimension_of_y*2, number_of_jobs)
+    return forest_learner
+
+
 def create_consistent_two_stream_regression_scaled_depth_delta_learner_32f(**kwargs):
     ux = float( kwargs.get('ux') )
     uy = float( kwargs.get('uy') )
@@ -170,6 +237,13 @@ def create_consistent_two_stream_regression_scaled_depth_delta_learner_32f(**kwa
 def create_vanilia_scaled_depth_delta_regression(**kwargs):
     return LearnerWrapper(  depth_delta_regression_data_prepare,
                             create_regression_scaled_depth_delta_learner_32f,
+                            create_regression_depth_delta_predictor_32f,
+                            kwargs)
+
+
+def create_biau2008_scaled_depth_delta_regression(**kwargs):
+    return LearnerWrapper(  depth_delta_regression_data_prepare,
+                            create_biau2008_regression_scaled_depth_delta_learner_32f,
                             create_regression_depth_delta_predictor_32f,
                             kwargs)
 
