3297c0955a5537bd23c42f2ef511cbac3a70db1f
David Matheson <david.j.matheson@gmail.com>
2013-08-28 19:04:40 -0700

    Renamed depth_delta_learner.py to classification_depth_delta_learner.py

--- modules/learn/python/__init__.py
+++ modules/learn/python/__init__.py
@@ -3,5 +3,5 @@ from wrappers import *
 from split_criteria import *
 from classification_matrix_learner import *
 from regression_matrix_learner import *
-from depth_delta_learner import *
+from classification_depth_delta_learner import *
 from regression_depth_delta_learner import *
